2c7550deb7b43f252fdd700cb99ab25be19f0d97
Conor-Code-Logistic-Tech <conor@code-logistic-tech.co.uk>
2025-08-07 16:52:59 +0000

    Add core dependencies and configure Express with Handlebars
    Why: Setting up modern Express foundation for TruBlu Financial presentation
    - Install express-handlebars to replace Jade for better template readability
    - Add helmet for security headers to protect against common vulnerabilities
    - Add compression middleware to improve page load performance for clients
    - Add nodemon for hot reload during development to speed up iteration
    - Configure Handlebars with partials support for component-based UI structure

--- app.js
+++ app.js
@@ -3,15 +3,27 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var helmet = require('helmet');
+var compression = require('compression');
+var exphbs = require('express-handlebars');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+// Security and performance middleware
+app.use(helmet());
+app.use(compression());
+
 // view engine setup
+app.engine('hbs', exphbs.engine({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  partialsDir: path.join(__dirname, 'views/partials')
+}));
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
